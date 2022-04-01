85186afac85c94ab8c32e50389aa8467bc45843e
Jake Chrumka <jake@recroom.com>
2022-04-01 13:31:51 -0400

    Cleaning up code

--- Assets/Runtime/Singleton.cs
+++ Assets/Runtime/Singleton.cs
@@ -1,6 +1,6 @@
 namespace Fizz6.Core
 {
-    public class Singleton<T> where T : class, new()
+    public class Singleton<T> where T : class, new ()
     {
         private static T _instance;
         public static T Instance => _instance ??= new T();
